6f875429f9411e0a784de4d6a33fa247cc0cb060
Khushant Wankhede <wankhedekhushant57@gmail.com>
2026-08-20 15:34:16 +0530

    added render link

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "https://ss-exam-portal.netlify.app",
-      "http://localhost:5000",
+      "https://ssexam.onrender.com",
       "https://exam.ssinfotech.co.in"
     ],
     credentials: true,
